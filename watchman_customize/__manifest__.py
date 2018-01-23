84ce26f64cd2566d80df34764e1a8900ec953428
angel moya <amoyapardo@gmail.com>
2018-01-23 17:45:33 +0100

    ADD customize

---
+++ watchman_customize/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Adaptación de Watchman",
+    "version": "11.0.1.0.0",
+    "author": "PESOL",
+    "website": "http://pesol.es",
+    "category": "Custom",
+    "license": "AGPL-3",
+    "depends": [
+        "mrp",
+    ],
+    "data": [
+        "views/mrp_view.xml",
+    ],
+    'installable': True,
+}
