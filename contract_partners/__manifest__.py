f1797a1f34c3ba09c0a0e5c3ac181c7d4e1165ca
angel moya <amoyapardo@gmail.com>
2018-02-07 13:32:48 +0100

    ADD partenr

---
+++ contract_partners/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Contract Partners',
+    'version': '0.1',
+    'author': 'PESOL',
+    'summary': 'Contract Partners',
+    'images': [],
+    'category': 'Sales Management',
+    'depends': [
+        'account',
+        'analytic'],
+    'data': [
+        'views/contract_view.xml',
+        #'views/invoice_view.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ contract_partners/models/contract.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+
+class AccountAnalyticAccount(models.Model):
+    _inherit = 'account.analytic.account'
+
+    service_partner_id = fields.Many2one(
+        'res.partner',
+        string="Service Address")
+
+    contact_partner_id = fields.Many2one(
+        'res.partner',
+        string="Contact Address")
+
+    def _prepare_invoice(self):
+        invoice = super(AccountAnalyticAccount,
+                        self)._prepare_invoice()
+        invoice.update({
+            'partner_shipping_id': self.service_partner_id
+                and self.service_partner_id.id)
+        return invoice
